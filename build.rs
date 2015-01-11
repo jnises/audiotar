bdf81eecf9e8564a176c8cea40c3f61f783fc2e3
Joel Nises <joel@nises.se>
2015-01-11 21:33:28 +0100

    more initial stuff

--- build.rs
+++ build.rs
@@ -1,1 +1,9 @@
-// adsf
+use std::io::Command;
+
+fn main() {
+    // let sndfileflags = match Command::new("pkg-config").arg("--libs").arg("sndfile").output() {
+    //     Ok(output) => output,
+    //     Err(e) => panic!("failed to get lib flags for sndfile. {}", e),
+    // };
+    // println!("cargo:rustc-flags={}", String::from_utf8_lossy(sndfileflags.output.as_slice()));
+}
